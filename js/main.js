cffa6f610963cbb389e25a1b9e93446db3e0ac00
Agustin Zocchi <agusz2003@gmail.com>
2022-10-11 23:09:02 -0300

    segunda entrega del proyecto

--- js/main.js
+++ js/main.js
@@ -1,64 +1,117 @@
 
 // declaracion de funciones variable
-let juegoseleccionado;
-
 let precioFIFA23 =69.99
 let precioNBA2k23 =69.99
-let precioSpidermanMilesMorales  =49.99
+let precioSpidermanMilesMorales =49.99
 let precioGodofWarRagnarök =69.99
 let precioDragonBallTheBreakers =19.99
 let precioNFLProEra =29.99
-let nombreIngresado 
+let saludoAlUsuario
 
-// declaracion de funciones
+
 function seleccionarJuego(){
-  juegoseleccionado= parseInt(prompt ("ingresa el numero del juego que buscas 1- FIFA 23     2-NBA 2k23      3-SPIDERMAN MILES MORALES       4-God of War Ragnarök       5-DRAGON BALL: THE BREAKERS      6-NFL PRO ERA"));
- }
-function pagar(precio){
-    let cuotas=parseInt(prompt("seleccione en cuantas cuotas quiere pagar (maximo 12 cuotas)"))
-      if(cuotas<=12){
-    let precioFinal= precio/cuotas
+  let producto =  parseInt (prompt("ingresa el numero del juego que buscas: 1-FIFA23 / 2-NBA2k23 / 3-SPIDERMAN MILES MORALES / 4-God of War Ragnarök / 5-DRAGON BALL: THE BREAKERS / 6-NFL PRO ERA"))
+    console.log(producto);
+
+       switch(producto) {
+         case 1: 
+         carrito.push(productos[0])
+         alert ("Usted eligió comprar el FIFA 23 con un valor de US$" + precioFIFA23)
+       pagar(precioFIFA23)
+       break;
+       case 2 :
+        carrito.push(productos[1])
+         alert ("Usted eligió comprar el NBA 2k23 con un valor de US$" + precioNBA2k23)
+       pagar(precioNBA2k23)
+       break;
+       case 3:
+        carrito.push(productos[2])
+         alert ("Usted eligió comprar el SPIDERMAN MILES MORALES con un valor de US$" + precioSpidermanMilesMorales )
+       pagar(precioSpidermanMilesMorales)
+       break;
+       case 4:
+        carrito.push(productos[3])
+       alert ("Usted eligió comprar el God of War Ragnarök con un valor de US$" + precioGodofWarRagnarök)
+         pagar(precioGodofWarRagnarök)
+         break;
+       case 5:
+        carrito.push(productos[4])
+         alert ("Usted eligió comprar el DRAGON BALL: THE BREAKERS con un valor de US$"+ precioDragonBallTheBreakers)
+        pagar(precioDragonBallTheBreakers)
+         break;
+       case 6:
+        carrito.push(productos[5])
+        alert ("Usted eligió comprar el NFL PRO ERA con un valor de US$" + precioNFLProEra)
+         pagar(precioNFLProEra)
+       break;
+       default:
+         alert ("ingese un numero valido")
+       break;
+        }
+    }
+
+ function pagar(precio){
+  
+  let unidades= parseInt(prompt("Cuantas unidades desea comprar (Maximo 5 por usuario)"))
+  let precioTotal = precio * unidades
+    alert ("seleccionaste " +  unidades + "unidades del juego. El valor final es  $" + precioTotal)
+  let cuotas=parseInt(prompt("seleccione en cuantas cuotas quiere pagar (maximo 6 cuotas)"))
+     if(cuotas<=6){
+  let precioFinal= precioTotal/cuotas
     alert ("el precio a pagar es US$"+ precioFinal + " por mes")
   }
-else{
+  else{
     alert ("Las cuotas seleccionadas no son validas")
   }
 }
-// ciclo
+
+
+let nombreIngresado 
 do{
-nombreIngresado = prompt ("registra tu nombre para inciar la compra")
- }while(nombreIngresado =="")
+   nombreIngresado = prompt ("registra tu nombre para inciar la compra")
+   }
+  while(nombreIngresado == "")
+  saludoAlUsuario = alert("hola " + nombreIngresado);
 
+  // objetos
+const productos = [
+  { id: 1, nombre: "FIFA23", precio: 69.99 },
+  { id: 2, nombre: "NBA2k23", precio: 69.99},
+  { id: 3, nombre: "SpidermanMilesMorales", precio: 49.99},
+  { id: 4, nombre: "GodofWarRagnarök", precio: 69.99},
+  { id: 5, nombre: "DragonBallTheBreakers", precio: 19.99},
+  { id: 6, nombre: "NFLProEra", precio: 29.99},
+ ];
+  let carrito = [];
 
+  let deseaContinuar = prompt("Esta interesado en comprar uno de nuestros productos? ingrese su respuesta si - no");
 
-// llamado de funcion
-seleccionarJuego()
-switch(juegoseleccionado){
-  case 1: 
-  alert ("El juego seleccionado es FIFA 23 con un valor de US$" + precioFIFA23)
-pagar(precioFIFA23)
-break;
-case 2:
-  alert ("El juego seleccionado es NBA 2k23 con un valor de US$" + precioNBA2k23)
-pagar(precioNBA2k23)
-break;
-case 3:
-  alert ("El juego seleccionado es SPIDERMAN MILES MORALES con un valor de US$" +precioSpidermanMilesMorales )
-pagar(precioSpidermanMilesMorales)
-break;
-case 4:
-alert ("El juego seleccionado es God of War Ragnarök con un valor de US$" + recioGodofWarRagnarök)
-  pagar(precioGodofWarRagnarök)
-  break;
-case 5:
-  alert ("El juego seleccionado es DRAGON BALL: THE BREAKERS con un valor de US$"+ recioDragonBallTheBreakers)
-  pagar(precioDragonBallTheBreakers)
-  break;
-case 6:
-  alert ("El juego seleccionado es NFL PRO ERA con un valor de US$" + precioNFLProEra)
-  pagar(precioNFLProEra)
-break;
-default:
-  alert ("ingese un numero valido")
-break;
-}
+  while(deseaContinuar != "si" && deseaContinuar != "no"){
+    alert("por favor responda con si o no")
+    deseaContinuar = prompt("Esta interesado en comprar uno de nuestros productos? ingrese su respuesta si - no")
+  };
+// metodo de filtrado del los arrays
+  if(deseaContinuar == "si"){
+    alert(" los juegos disponibles por el momento son")
+    let productosDisponibles = productos.map(
+      (producto) => producto.nombre + " " + producto.precio + "$");
+      alert(productosDisponibles.join(" - "))
+  } else if (deseaContinuar =="no"){
+    alert("disfrute el sitio, adios")
+  }
+  
+  seleccionarJuego()
+  deseaContinuar = prompt("desea comprar otro juego")
+  do{
+    seleccionarJuego()
+    deseaContinuar = prompt("desea comprar otro juego")
+  }
+  while(deseaContinuar === "si")
+  
+  console.log(carrito)
+  
+
+      
+    
+
+     
